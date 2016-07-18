26f9871e4f6f6cf6bb89f9cb9db59abb6b8394a8
Sneha Inguva <singuva@digitalocean.com>
2016-07-18 18:07:56 -0400

    have separate process to analyze links

--- main.py
+++ main.py
@@ -2,17 +2,30 @@
 import time
 import json
 from slackclient import SlackClient
+import multiprocessing
 
+#worker checks for https link and prints/adds to db if that's the case
+def worker(msgs):
+    """thread worker function"""
+    if msgs["type"] == "message":
+        #check if message contains a link
+        print 'Worker:', msgs
+    return
 
-with open("config.json") as fd:
-    config = json.load(fd)
+if __name__ == "__main__":
+    with open("config.json") as fd:
+        config = json.load(fd)
 
-print config
-sc = SlackClient(config["slack_token"])
+    sc = SlackClient(config["slack_token"])
 
-if sc.rtm_connect():
-    while True:
-        print sc.rtm_read()
-        time.sleep(1)
-else:
-    print "Connection Failed, invalid token?"
+    jobs = [] #should i remove the jobs from a queue? 
+    if sc.rtm_connect():
+        while True:
+            line = sc.rtm_read()
+            if len(line) > 0:
+                 p = multiprocessing.Process(target=worker, args=(line))
+                 jobs.append(p)
+                 p.start()
+            time.sleep(1)
+    else:
+        print "Connection Failed, invalid token?"
